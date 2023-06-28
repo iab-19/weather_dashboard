1bcea5bb5eae1526b67d5582d874f82fad4fa0e1
Ibrahima <iab.jr10@gmail.com>
2023-06-27 20:27:08 -0400

    Update HTML file

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 const APIKey = 'cb4f2678290f12a8cbfdfaa638ab9e93';
 var city;
+city = 'London';
+// city = document.getElementById('searchBar');
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+
+var data = fetch(queryURL);
+console.log(data);
+fetch(queryURL);
