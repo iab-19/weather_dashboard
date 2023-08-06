e59f998986cbab2cb5c254bc39f4f5fc9aff03e8
Ibrahima <iab.jr10@gmail.com>
2023-08-05 23:05:32 -0400

    Change color of background and nav bar. Get temperature weather description.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,30 @@
-const APIKey = 'cb4f2678290f12a8cbfdfaa638ab9e93';
+const APIKey = 'cb4f2678290f12a8cbfdfaa638ab9e93'; // API key
+const submitButton = document.getElementById('submitButton'); // Submit button
+const textBox = document.getElementById('searchBar'); // Text box
 var city;
-city = 'London';
+city = 'clarksburg';
 // city = document.getElementById('searchBar');
-var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+console.log(queryURL);
 
+// function getURL() {
+    fetch(queryURL)
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        const temperature = data.main.temp;
+        const weatherDescription = data.weather[0].description;
+        console.log(`Temperature: ${temperature}F`);
+        console.log(`Weather Description: ${weatherDescription}`);
+    })
+    .catch(err => alert("Wrong city name"))
+    console.log(queryURL);
+// }
 
-fetch(queryURL)
-    .then(response => response.json())
-    .then(data => console.log(data))
-.catch(err => alert("Wrong city name"))
+// submitButton.addEventListener('click', getURL);
+// console.log(data.weather.main)
